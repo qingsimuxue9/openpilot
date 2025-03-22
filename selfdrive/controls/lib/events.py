c952f4d66146dbe803c6149c894073c54efb98a2
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-03-22 22:28:52 +0800

    修改事件提示

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -192,7 +192,7 @@ class NormalPermanentAlert(Alert):
 
 
 class StartupAlert(Alert):
-  def __init__(self, alert_text_1: str, alert_text_2: str = "请注意路况，安全驾驶，这不是自动驾驶", alert_status=AlertStatus.normal):
+  def __init__(self, alert_text_1: str, alert_text_2: str = "道路千万条，安全第一条", alert_status=AlertStatus.normal):
     super().__init__(alert_text_1, alert_text_2,
                      alert_status, AlertSize.mid,
                      Priority.LOWER, VisualAlert.none, AudibleAlert.none, 5.),
