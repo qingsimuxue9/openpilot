73a88c3ee8e2fa5fbdf3ed12c00015ae5cb4247c
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-03-31 09:00:30 +0800

    修改跟车距离在4种之间循环

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -720,7 +720,7 @@ class Controls:
     # decrement personality on distance button press
     if self.CP.openpilotLongitudinalControl:
       if any(not be.pressed and be.type == ButtonType.gapAdjustCruise for be in CS.buttonEvents):
-        self.personality = (self.personality - 1) % 3
+        self.personality = (self.personality - 1) % 4
         self.params.put_nonblocking('LongitudinalPersonality', str(self.personality))
 
     return CC, lac_log
