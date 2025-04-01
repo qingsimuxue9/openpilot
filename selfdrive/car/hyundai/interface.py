f2c95934f9c8bc1cda9ca9222161572258c5db79
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-04-01 19:25:25 +0800

    修改vEgoStopping=0.5

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -184,7 +184,7 @@ class CarInterface(CarInterfaceBase):
 
     #if Params().get_bool("HkgSmoothStop"):
     #  ret.vEgoStopping = 0.1
-    ret.vEgoStopping = 0.1
+    ret.vEgoStopping = 0.5
 
     return ret
 
